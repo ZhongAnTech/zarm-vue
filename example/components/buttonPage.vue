c4c311058a186d0012a9d914473cd2a17db812c6
chrisHchen <chenchris1986@163.com>
2017-08-15 16:22:14 +0800

    add button

--- example/components/buttonPage.vue
+++ example/components/buttonPage.vue
@@ -8,9 +8,87 @@
             <za-panel-title>基本</za-panel-title>
           </za-panel-header>
           <za-panel-body>
-
+            <za-button theme="primary">普通按钮</za-button>
+            <za-button theme="primary" block>块级按钮</za-button>
+            <za-button theme="primary" block active>激活状态的按钮</za-button>
+            <za-button theme="primary" block disabled>禁用状态的按钮</za-button>
           </za-panel-body>
         </za-panel>
+
+        <za-panel>
+          <za-panel-header>
+            <za-panel-title>幽灵按钮</za-panel-title>
+          </za-panel-header>
+          <za-panel-body>
+            <za-button block bordered>幽灵按钮</za-button>
+            <za-button block bordered active>激活状态的按钮</za-button>
+            <za-button block bordered disabled>禁用状态的按钮</za-button>
+            <za-button theme="primary" block bordered>幽灵按钮</za-button>
+            <za-button theme="primary" block bordered active>激活状态的按钮</za-button>
+            <za-button theme="primary" block bordered disabled>禁用状态的按钮</za-button>
+          </za-panel-body>
+        </za-panel>
+
+        <za-panel>
+          <za-panel-header>
+            <za-panel-title>多主题</za-panel-title>
+          </za-panel-header>
+          <za-panel-body>
+            <za-button>Default</za-button>
+            <za-button theme="primary">primary</za-button>
+            <za-button theme="info">info</za-button>
+            <za-button theme="success">success</za-button>
+            <za-button theme="warning">warning</za-button>
+            <za-button theme="error">error</za-button>
+          </za-panel-body>
+        </za-panel>
+
+        <za-panel>
+          <za-panel-header>
+            <za-panel-title>按钮大小</za-panel-title>
+          </za-panel-header>
+          <za-panel-body>
+            <za-button theme="primary" size='xl'>xl</za-button>
+            <za-button theme="primary" size='lg'>lg</za-button>
+            <za-button theme="primary">md</za-button>
+            <za-button theme="primary" size='sm'>sm</za-button>
+            <za-button theme="primary" size='xs'>xs</za-button>
+          </za-panel-body>
+        </za-panel>
+
+        <za-panel>
+          <za-panel-header>
+            <za-panel-title>圆角、椭圆角、圆形按钮</za-panel-title>
+          </za-panel-header>
+          <za-panel-body>
+            <za-button bordered shape="radius">圆角按钮</za-button>
+            <za-button bordered shape="round">椭圆角按钮</za-button>
+            <za-button bordered shape="circle">GO</za-button>
+            <za-button bordered shape="circle">
+              <za-icon type='right' slot='icon'></za-icon>
+            </za-button>
+          </za-panel-body>
+        </za-panel>
+
+        <za-panel>
+          <za-panel-header>
+            <za-panel-title>带icon的按钮</za-panel-title>
+          </za-panel-header>
+          <za-panel-body>
+            <za-button bordered shape="radius">
+              <za-icon type='right-round' theme='success' slot='icon'></za-icon>
+              正确
+            </za-button>
+            <za-button bordered shape="radius">
+              <za-icon type='wrong-round' theme='error' slot='icon'></za-icon>
+              错误
+            </za-button>
+            <za-button bordered loading shape="radius">
+              加载中
+            </za-button>
+          </za-panel-body>
+        </za-panel>
+
       </div>
     </main>
     <PageFooter />
--- src/button/src/button.vue
+++ src/button/src/button.vue
@@ -1,14 +1,83 @@
 <template lang="html">
-
+  <a :class='{
+    [`${prefixCls}`]: true,
+    [`theme-${theme}`]: !!theme,
+    [`size-${size}`]: !!size,
+    [`shape-${shape}`]: !!shape,
+    block,
+    bordered,
+    active,
+    focus,
+    disabled,
+  }'>
+    <span :class='`${prefixCls}-content`'>
+      <za-spinner class="rotate360" v-if='loading'/>
+      <slot name='icon' v-else></slot>
+      <span>
+        <slot></slot>
+      </span>
+    </span>
+  </a>
 </template>
 
 <script>
+// necessary when used alone
+import zaSpinner from '@/spinner';
+
 export default {
   name: 'zaButton',
+  components: {
+    zaSpinner,
+  },
   props: {
     prefixCls: {
       type: String,
-      default: 'za-cell',
+      default: 'za-button',
+    },
+    theme: {
+      type: String,
+      validator: function (v) { // eslint-disable-line object-shorthand
+        return ['default', 'primary', 'info', 'success', 'warning', 'error'].indexOf(v) >= 0;
+      },
+      default: 'default',
+    },
+    size: {
+      type: String,
+      validator: function (v) { // eslint-disable-line object-shorthand
+        return ['xl', 'lg', 'sm', 'xs'].indexOf(v) >= 0;
+      },
+      default: null,
+    },
+    shape: {
+      type: String,
+      validator: function (v) { // eslint-disable-line object-shorthand
+        return ['radius', 'round', 'circle'].indexOf(v) >= 0;
+      },
+      default: null,
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+    bordered: {
+      type: Boolean,
+      default: false,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    focus: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
     },
   },
   data() {
