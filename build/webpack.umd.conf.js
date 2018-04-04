0be738754d793cc0e5907d1179dfc1ae0de26586
chuanshuoye <232465450@qq.com>
2018-04-04 12:35:52 +0800

    Fixed umd  vue.extend  unknown error

--- build/webpack.umd.conf.js
+++ build/webpack.umd.conf.js
@@ -20,9 +20,20 @@ module.exports = merge(baseWebpackConfig, {
   entry: {
     ['zarm-vue.umd.js']: path.join(__dirname, '../src/index.js'),
   },
-  externals: [
-    'vue',
-  ],
+  externals: {
+    'vue': {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    },
+    'lodash': {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
+    }
+  },
   output: {
     path: path.resolve(__dirname, '../release'),
     filename: '[name]',
