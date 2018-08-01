7f2bfa8981075bc2c72f6de696243ef73b0ff094
叶展旺 <yezhanwang@Ares.local>
2018-08-01 19:09:00 +0800

    test: jest unit

---
+++ test/unit/jest.conf.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../../'),
+  moduleFileExtensions: ['js', 'vue'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+  },
+  testPathIgnorePatterns: ['<rootDir>/test/e2e'],
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  setupFiles: ['<rootDir>/test/unit/setup'],
+  mapCoverage: true,
+  collectCoverage: true,
+  coverageReporters: ['html', 'lcov', 'text-summary'],
+  coverageDirectory: '<rootDir>/test/unit/coverage',
+  collectCoverageFrom: ['src/**/*.{js,vue}', '!**/node_modules/**'],
+};
