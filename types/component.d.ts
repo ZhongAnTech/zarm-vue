f0f0762608af61074923a5faa47f402cc7c37a29
叶展旺 <yezhanwang@Ares.local>
2018-09-11 10:34:53 +0800

    feat: add typescript typing

---
+++ types/component.d.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+
+export class ZarmComponent {
+  static name: string;
+  static install(vue: typeof Vue): void;
+}
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import { ZarmComponent } from './component';
+
+export function install (vue: typeof Vue): void
+
+export class Button extends ZarmComponent {}
