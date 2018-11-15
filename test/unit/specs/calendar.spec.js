6157e4d2df20135d691f69028e28a9b737fba69f
李康捷 <likangjie@zhongan.com>
2018-11-15 12:02:37 +0800

    feat(calendar): add test bug error

--- test/unit/specs/calendar.spec.js
+++ test/unit/specs/calendar.spec.js
@@ -2,13 +2,13 @@ import zaCalendar from '@/calendar';
 import { mount } from '../util';
 
 describe('Calendar', () => {
-  it('create', () => {
+  it('create', done => {
     const wrapper = mount(zaCalendar);
     const { vm } = wrapper;
     expect(vm.$el.classList.contains('za-calendar')).toBe(true);
   });
 
-  it('set selectedValue', done => {
+  it('set sigleValue', done => {
     const TestCompo = {
       components: {
         zaCalendar,
@@ -16,76 +16,56 @@ describe('Calendar', () => {
       template: `
         <za-calendar 
           ref='calendar'
-          :visible.sync='visible' 
-          @changed='changeDate' 
-          @ok='handleOk1' 
-          :selectedValue='value1' 
+          v-model="value" 
         />
       `,
       data() {
         return {
-          visible: false,
-          value1: [],
+          value: '',
         };
       },
       methods: {
-        changeDate(date) {
-          this.value1 = date;
-        },
-        handleOk1(date) {
-          this.value1 = date;
-        },
       },
     };
     const wrapper = mount(TestCompo);
     const { vm } = wrapper;
-    vm.visible = true;
-    vm.$refs.calendar.setChanged('2018-02-23');
+    const calendar = vm.$refs.calendar;
+    const selectedDate = calendar.monthList[0].dates[10];
+    calendar._dateClick(selectedDate);
     vm.$nextTick(() => {
-      expect(vm.value1[0]).toEqual('2018-02-23');
+      expect(vm.value).toBe(selectedDate.date);
       done();
     });
   });
 
-  it('set selectedRangeValue', done => {
+  it('set multipleValue', done => {
     const TestCompo = {
       components: {
         zaCalendar,
       },
       template: `
-        <za-calendar 
-         ref='calendar'
-         :multiSelected='isMultiSelected'
-         :visible.sync='visible' 
-         @changed='changeDate' 
-         @ok='handleOk1' 
-         :selectedValue='value1' 
-        />
+      <za-calendar 
+        ref='calendar'
+        v-model="value" 
+        multiple
+      />
       `,
       data() {
         return {
-          visible: false,
-          isMultiSelected: true,
-          value1: [],
+          value: '',
         };
-      },
-      methods: {
-        changeDate(date) {
-          this.value1 = date;
-        },
-        handleOk1(date) {
-          this.value1 = date;
-        },
       },
     };
     const wrapper = mount(TestCompo);
     const { vm } = wrapper;
-    vm.visible = true;
-    vm.$refs.calendar.setChanged('2018-02-23');
-    vm.$refs.calendar.setChanged('2018-03-20');
+    const calendar = vm.$refs.calendar;
+    const selectedDate = [calendar.monthList[0].dates[10], calendar.monthList[0].dates[20]];
+    calendar._dateClick(selectedDate[0]);
+    calendar._dateClick(selectedDate[1]);
     vm.$nextTick(() => {
-      expect(vm.value1[0]).toEqual('2018-02-23');
-      expect(vm.value1[1]).toEqual('2018-03-20');
+      expect(vm.value[0]).toBe(selectedDate[0].date);
+      expect(vm.value[1]).toBe(selectedDate[1].date);
+      expect(vm.$el.querySelectorAll('inrange').length).toBe(11);
       done();
     });
   });
