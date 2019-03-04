2f76becc6d911374841d6e1dbd8b949ede4f0fe2
zhanwang.ye <232465450@qq.com>
2019-03-04 16:42:10 +0800

    refactor: bem css modify

--- script/css-entry.js
+++ script/css-entry.js
@@ -1,16 +1,19 @@
 const Glob = require('glob');
 const path = require('path');
+const chalk = require('chalk');
 
 module.exports = function () {
   const entries = {};
   const partten = path.join(__dirname, '../src/**/component.js');
   try {
+    console.log(chalk.blue('build components theme css start'));
     Glob.sync(partten, {}).forEach(file => {
       const pathArray = file.split('/');
       const key = pathArray[pathArray.length - 3];
+      console.log(chalk.yellow(`${key}: ${file}`));
       entries[key] = file;
     });
-    console.log('build component css start');
+    console.log('');
     return entries;
   } catch (err) {
     throw new Error(err.message);
