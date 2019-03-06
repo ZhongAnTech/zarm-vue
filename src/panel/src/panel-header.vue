6ebecc7488c37dcd50c3c09482c26e54e2507e02
zhanwang.ye <232465450@qq.com>
2019-03-06 11:55:05 +0800

    refactor: panel, message, navbar updated to bem css

--- src/panel/src/panel-header.vue
+++ src/panel/src/panel-header.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <div :class='`${prefixCls}-header`'>
-    <div :class='`${prefixCls}-title`' v-if='$slots.title || title'>
+  <div :class='`${prefixCls}__header`'>
+    <div :class='`${prefixCls}__title`' v-if='$slots.title || title'>
       <slot name='title'></slot>
       <template v-if="!$slots.title">{{title}}</template>
     </div>
-    <div :class='`${prefixCls}-more`' v-if='$slots.more || more'>
+    <div :class='`${prefixCls}__more`' v-if='$slots.more || more'>
       <slot name='more'></slot>
       <template v-if="!$slots.more">{{more}}</template>
     </div>
