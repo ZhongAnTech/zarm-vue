3445efb4024bec46d2331981dc47df7bc9ea74c9
zhanwang.ye <232465450@qq.com>
2019-04-19 11:51:39 +0800

    feat: typescript component

--- script/find-entry.js
+++ script/find-entry.js
@@ -4,10 +4,11 @@ const chalk = require('chalk');
 
 module.exports = function () {
   const entries = {};
-  const partten = path.join(__dirname, '../src/**/index.js');
+  const parttenJS = path.join(__dirname, '../src/**/index.js');
+  const parttenTS = path.join(__dirname, '../src/**/index.ts');
   try {
     console.log(chalk.blue('build components entries start'));
-    Glob.sync(partten, {}).forEach((file) => {
+    Glob.sync(parttenJS, {}).forEach((file) => {
       if (file.indexOf('src/index.js') >= 0) {
         entries['zarm-vue.common'] = file;
       } else {
@@ -16,6 +17,12 @@ module.exports = function () {
         console.log(chalk.yellow(`${key}: ${file}`));
         entries[key] = file;
       }
+    });
+    Glob.sync(parttenTS, {}).forEach((file) => {
+      const pathArray = file.split('/');
+      const key = pathArray[pathArray.length - 2];
+      console.log(chalk.yellow(`${key}: ${file}`));
+      entries[key] = file;
     });
     console.log('');
     return entries;
