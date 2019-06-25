a553187a633ce8616a2c9c34124ffc84c1ef7e43
zhanwang.ye <232465450@qq.com>
2019-06-25 15:15:48 +0800

    chore: babel6 updateTo babel7

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -12,7 +12,6 @@ module.exports = {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
   },
-  // testMatch: ['<rootDir>/test/unit/specs/loading.spec.js'],
   testPathIgnorePatterns: ['<rootDir>/test/e2e'],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
