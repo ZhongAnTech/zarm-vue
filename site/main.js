fbf6d9fca4027b661c884ed04688a76c0a2978f7
chuanshuoye <232465450@qq.com>
2020-10-22 14:35:36 +0800

    docs: update site

--- site/main.js
+++ site/main.js
@@ -1,14 +1,67 @@
+import 'normalize.css';
+import 'highlight.js/styles/github-gist.css';
+import Perfume from 'perfume.js';
 import { createApp } from 'vue';
 import App from './app';
 import zarmVue from '../src/index';
 import router from './router';
 import store from './store';
-// eslint-disable-next-line import/first
-import 'normalize.css';
-// eslint-disable-next-line import/first
-import 'highlight.js/styles/github-gist.css';
 import '../src/badge/style/index.scss';
 import './styles/index.scss';
+
+const perfume = new Perfume({
+  analyticsTracker: (options) => {
+    const { metricName, data, navigatorInformation } = options;
+    switch (metricName) {
+      case 'navigationTiming':
+        if (data && data.timeToFirstByte) {
+          console.log('navigationTiming', data);
+        }
+        break;
+      case 'networkInformation':
+        if (data && data.effectiveType) {
+          console.log('networkInformation', data);
+        }
+        break;
+      case 'fp':
+        console.log('firstPaint', { duration: data });
+        break;
+      case 'fcp':
+        console.log('firstContentfulPaint', { duration: data });
+        break;
+      case 'fid':
+        console.log('firstInputDelay', { duration: data });
+        break;
+      case 'lcp':
+        console.log('largestContentfulPaint', { duration: data });
+        break;
+      case 'lcpFinal':
+        console.log('largestContentfulPaintFinal', { duration: data });
+        break;
+      case 'cls':
+        console.log('cumulativeLayoutShift', { duration: data });
+        break;
+      case 'clsFinal':
+        console.log('cumulativeLayoutShiftFinal', { duration: data });
+        break;
+      case 'tbt':
+        console.log('totalBlockingTime', { duration: data });
+        break;
+      case 'tbt5S':
+        console.log('totalBlockingTime5S', { duration: data });
+        break;
+      case 'tbt10S':
+        console.log('totalBlockingTime10S', { duration: data });
+        break;
+      case 'tbtFinal':
+        console.log('totalBlockingTimeFinal', { duration: data });
+        break;
+      default:
+        console.log(metricName, { duration: data });
+        break;
+    }
+  },
+});
 
 window.app = createApp(App);
 window.app.use(zarmVue);
